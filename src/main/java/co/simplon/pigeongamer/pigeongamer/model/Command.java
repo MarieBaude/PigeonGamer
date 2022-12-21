fc8fce5696f6efcc78d2f19838d1c1da3e39f841
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-12-21 15:03:39 +0100

    del a model

--- src/main/java/co/simplon/pigeongamer/pigeongamer/model/Command.java
+++ src/main/java/co/simplon/pigeongamer/pigeongamer/model/Command.java
@@ -28,4 +28,62 @@ public class Command {
 	@OneToMany
 	@JoinColumn(name = "id_product")
 	private Product product;
+
+	/**
+	 * @return the id_command
+	 */
+	public long getId_command() {
+		return id_command;
+	}
+
+	/**
+	 * @param id_command the id_command to set
+	 */
+	public void setId_command(long id_command) {
+		this.id_command = id_command;
+	}
+
+	/**
+	 * @return the command_date
+	 */
+	public LocalDate getCommand_date() {
+		return command_date;
+	}
+
+	/**
+	 * @param command_date the command_date to set
+	 */
+	public void setCommand_date(LocalDate command_date) {
+		this.command_date = command_date;
+	}
+
+	/**
+	 * @return the command_delivery
+	 */
+	public LocalDate getCommand_delivery() {
+		return command_delivery;
+	}
+
+	/**
+	 * @param command_delivery the command_delivery to set
+	 */
+	public void setCommand_delivery(LocalDate command_delivery) {
+		this.command_delivery = command_delivery;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
 }
--- src/main/java/co/simplon/pigeongamer/pigeongamer/model/Historical.java
+++ src/main/java/co/simplon/pigeongamer/pigeongamer/model/Historical.java
@@ -32,5 +32,77 @@ public class Historical {
 	@OneToOne
 	@JoinColumn(name = "id_supplier")
 	private Supplier supplier;
+
+	/**
+	 * @return the id_historical
+	 */
+	public long getId_historical() {
+		return id_historical;
+	}
+
+	/**
+	 * @param id_historical the id_historical to set
+	 */
+	public void setId_historical(long id_historical) {
+		this.id_historical = id_historical;
+	}
+
+	/**
+	 * @return the historical_command_date
+	 */
+	public LocalDate getHistorical_command_date() {
+		return historical_command_date;
+	}
+
+	/**
+	 * @param historical_command_date the historical_command_date to set
+	 */
+	public void setHistorical_command_date(LocalDate historical_command_date) {
+		this.historical_command_date = historical_command_date;
+	}
+
+	/**
+	 * @return the historical_command_delivery
+	 */
+	public LocalDate getHistorical_command_delivery() {
+		return historical_command_delivery;
+	}
+
+	/**
+	 * @param historical_command_delivery the historical_command_delivery to set
+	 */
+	public void setHistorical_command_delivery(LocalDate historical_command_delivery) {
+		this.historical_command_delivery = historical_command_delivery;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the supplier
+	 */
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	/**
+	 * @param supplier the supplier to set
+	 */
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	
+	
 }
 
