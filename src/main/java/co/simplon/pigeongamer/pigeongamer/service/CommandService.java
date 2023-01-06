e43e5c2fd4cf49445523e871baca50899545ac45
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-06 18:08:51 +0100

    adding command service java doc

--- src/main/java/co/simplon/pigeongamer/pigeongamer/service/CommandService.java
+++ src/main/java/co/simplon/pigeongamer/pigeongamer/service/CommandService.java
@@ -5,8 +5,29 @@ import java.util.List;
 import co.simplon.pigeongamer.pigeongamer.model.Command;
 import co.simplon.pigeongamer.pigeongamer.model.Product;
 
+/**
+ * Provides services for managing commands.
+ */
 public interface CommandService {
+	/**
+     * Retrieves a list of all commands.
+     *
+     * @return A list of all commands.
+     */
 	List < Command > getAllCommands();
+	
+	 /**
+     * Retrieves a command by its ID.
+     *
+     * @param id_command The ID of the command to retrieve.
+     * @return The command with the specified ID, or null if no such command exists.
+     */
     Command getCommandById(long id_command);
+    
+    /**
+     * Saves a new command.
+     *
+     * @param productList The list of products in the command.
+     */
     void saveCommand(List<Product> productList);
-	}
+}
