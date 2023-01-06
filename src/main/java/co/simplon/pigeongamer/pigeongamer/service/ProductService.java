25e13a78162ed053b89eb0bca0a991f31437425c
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-06 18:20:42 +0100

    adding javadoc for product service

--- src/main/java/co/simplon/pigeongamer/pigeongamer/service/ProductService.java
+++ src/main/java/co/simplon/pigeongamer/pigeongamer/service/ProductService.java
@@ -4,9 +4,36 @@ import java.util.List;
 
 import co.simplon.pigeongamer.pigeongamer.model.Product;
 
+/**
+ * Defines a service for managing products.
+ */
 public interface ProductService {
+	/**
+	 * Retrieves a list of all products.
+	 *
+	 * @return a list of all products
+	 */
 	List < Product > getAllProducts();
+	
+	/**
+	 * Saves a new product or updates an existing one.
+	 *
+	 * @param product the product to save
+	 */
     void saveProduct(Product product);
+    
+	/**
+	 * Retrieves the product with the given ID.
+	 *
+	 * @param id_product the ID of the product to retrieve
+	 * @return the product with the given ID, or {@code null} if no such product exists
+	 */
     Product getProductById(long id_product);
+    
+	/**
+	 * Deletes the product with the given ID.
+	 *
+	 * @param id_product the ID of the product to delete
+	 */
     void deleteProductById(long id_product);
 }
